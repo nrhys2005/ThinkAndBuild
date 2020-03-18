6d41e0456dee887633c658018370d63508c0017a
Hyeonseok0830 <96hyeonseok@gmail.com>
2020-03-18 21:05:20 +0900

    카메라 오브젝트
    awsd로 캐릭터 이동 , 카메라 캐릭터 따라다님

---
+++ 3DTaB/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+    public GameObject player;
+
+    public float offsetX = 5f;
+    public float offsetY = 5f;
+    public float offsetZ = -35f;
+
+    public float followSpeed = 2;
+    Vector3 cameraPosition;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        cameraPosition.x = player.transform.position.x + offsetX;
+        cameraPosition.y = player.transform.position.y + offsetY;
+        cameraPosition.z = player.transform.position.z + offsetZ;
+
+        transform.position = cameraPosition;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, followSpeed * Time.deltaTime);
+    }
+}
---
+++ TaB/Assets/Scripts/CharacterWalk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterWalk : MonoBehaviour {
+
+    public float maxSpeed;
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();        
+    }
+
+    private void FixedUpdate()
+    {
+        //Move By Key Control
+        float h = Input.GetAxis("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        else if (rigid.velocity.x < maxSpeed*(-1))
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
+    }
+}
