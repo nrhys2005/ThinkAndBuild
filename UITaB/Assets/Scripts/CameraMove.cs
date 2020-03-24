befb5199455742805b10e270aa324a801a6fd47b
Hyeonseok0830 <96hyeonseok@gmail.com>
2020-03-24 19:15:42 +0900

    UI

---
+++ UITaB/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int speed = 10;
+
+    // Use this for initialization
+
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        moveObject();
+    }
+    void moveObject()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            transform.Translate(Vector3.left * speed * Time.smoothDeltaTime * mouseX, Space.World);
+            transform.Translate(Vector3.forward * speed * Time.smoothDeltaTime * mouseY, Space.World);
+        }
+    }
+}
---
+++ UITaB/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public Animator animator;
+    Vector3 target = new Vector3(8, 1.5f, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 velo = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.5f);
+
+        
+    }
+    private void FixedUpdate()
+    {
+       
+    }
+
+    private void Move()
+    {
+       
+
+      
+    }
+}
