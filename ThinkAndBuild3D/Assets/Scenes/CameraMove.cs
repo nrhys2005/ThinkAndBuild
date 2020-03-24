9031ac54804926bf11953cc07a6178cf784fe71b
nrhys2005 <56465527+nrhys2005@users.noreply.github.com>
2020-03-24 18:56:23 +0900

    파일 정리 및 배경업데이트

---
+++ ThinkAndBuild3D/Assets/Scenes/CameraMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private Camera mainCamera;
+    public float followSpeed = 10;
+    public float zoomSpeed = 10.0f;
+    
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        moveObject();
+        Zoom();
+        
+    }
+    private void Zoom()
+    {
+        float distance = Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed;
+        if (distance != 0)
+        {
+            mainCamera.fieldOfView += distance;
+        }
+    }
+    
+    
+    void moveObject()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            transform.Translate(Vector3.left * followSpeed * Time.smoothDeltaTime * mouseX, Space.World);
+            transform.Translate(Vector3.forward * followSpeed * Time.smoothDeltaTime * mouseY, Space.World);
+        }
+    }
+}
