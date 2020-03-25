18dc6f2a880a5a8b118a7a57679878f9734b7916
Hyeonseok0830 <96hyeonseok@gmail.com>
2020-03-25 20:22:45 +0900

    2020-03-25
    커밋제발되라

---
+++ UITaB/Assets/Scripts/Behavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 3.0f;
+    private Rigidbody rigid;
+    private Animator anim;
+
+
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    public bool Run(Vector3 targetPos)
+    {
+        // 이동하고자하는 좌표 값과 현재 내 위치의 차이를 구한다.
+        float dis = Vector3.Distance(transform.position, targetPos);
+        if (dis >= 1.0f) // 차이가 아직 있다면
+        {
+            // 캐릭터를 이동시킨다.
+            transform.localPosition = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+          
+            anim.SetBool("isWalk",true); // 걷기 애니메이션 처리
+            return true;
+        }
+        else
+        {
+            anim.SetBool("isWalk", false); // 걷기 애니메이션 처리
+        }
+        return false;
+
+    }
+
+    public void Turn(Vector3 targetPos)
+    {
+        // 캐릭터를 이동하고자 하는 좌표값 방향으로 회전시킨다
+        Vector3 dir = targetPos - transform.position;
+        Vector3 dirXZ = new Vector3(dir.x, 0, dir.y);
+        Quaternion targetRot = Quaternion.LookRotation(dirXZ);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, 180.0f * Time.deltaTime);
+
+       
+    }
+}
---
+++ UITaB/Assets/Scripts/Control.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Behavior behavior; // 캐릭터의 행동 스크립트
+    private Camera mainCamera; // 메인 카메라
+    private Vector3 targetPos; // 캐릭터의 이동 타겟 위치
+    private Animator anim;
+
+    void Start()
+    {
+        behavior = GetComponent<Behavior>();
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        
+        // 마우스 입력을 받았 을 때
+        if (Input.GetMouseButtonUp(1))
+        {
+            // 마우스로 찍은 위치의 좌표 값을 가져온다
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000f))
+            {
+                targetPos = hit.point;
+            }
+            //anim.SetBool("isWalk", true);
+        }
+        // 캐릭터가 움직이고 있다면
+        if (behavior.Run(targetPos))
+        {
+            // 회전도 같이 시켜준다            
+            behavior.Turn(targetPos);
+        }
+        else
+        {  
+            //anim.SetBool("isWalk", false); // 걷기 애니메이션 처리
+        }
+    }
+}
